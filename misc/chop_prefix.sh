9b2cd3ef8b9551c70522a9cdc4a0503ccd9b08bd
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-01-06 22:09:50 -0800

    renamed all prefix files.
    All files starting with a letter, number and underscore have
    had the prefix removed. The html files with the prefix still
    exist, but they contain a redirect to the actual files to preserve
    any sort of backwards compatibility for users if they bookmarked
    pages.

--- misc/chop_prefix.sh
+++ misc/chop_prefix.sh
@@ -19,21 +19,21 @@ do
    		oldfiles=${oldrmd}_${filesdir}
    		newfiles=${newrmd}_${filesdir}   		
    		printf "cp $name -> $newname\n"
-   		# cp $name $newname
+   		cp $name $newname
    		printf "cp $oldrmd.Rmd -> $newrmd.Rmd\n"
-   		# cp $oldrmd.Rmd $newrmd.Rmd
+   		cp $oldrmd.Rmd $newrmd.Rmd
    		printf "cp $name -> old/$name\n"
-   		# cp $name old/$name
+   		cp $name old/$name
    		printf "mv $oldrmd.Rmd -> old/$oldrmd.Rmd\n"
-   		# mv $oldrmd.Rmd old/$oldrmd.Rmd
+   		mv $oldrmd.Rmd old/$oldrmd.Rmd
    		printf "mv $oldcache/ -> $newcache/\n"
-   		# mv $oldcache/ $newcache/
+   		mv $oldcache/ $newcache/
    		printf "mv $oldfiles/ -> $newfiles/\n"
-   		# mv $oldfiles/ $newfiles/
+   		mv $oldfiles/ $newfiles/
    		printf "redirect $name -> $newname\n"
-   		# cp $redirect $name
-	   	# perl -p -i -e 's/http:.+?in_R\//'$newname'/g' $name
-	   	# perl -p -i -e 's/(A|C)[[:digit:]]+?_//g'$newrmd.Rmd
+   		cp $redirect $name
+	   	perl -p -i -e 's/http:.+?in_R\//'$newname'/g' $name
+	   	perl -p -i -e 's/(A|C)[[:digit:]]+?_//g' $newrmd.Rmd
    	else
    		printf "$name\n"
    	fi
