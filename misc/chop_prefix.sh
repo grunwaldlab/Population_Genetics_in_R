8f935dd13eacb577c055bb6248e25ec68d459036
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-01-06 21:12:52 -0800

    modify chop_prefix

--- misc/chop_prefix.sh
+++ misc/chop_prefix.sh
@@ -7,25 +7,26 @@ while read line
 do
     name=$line
     newname=`printf $name | cut -d _ -f 2-`
-    printf "$newname\n"
+    printf "====================\n"
    	if [[ $name != $newname ]]
    	then
-   		printf "$name -> old/$name\n"
+   		oldrmd=`printf $name | cut -d \. -f 1`
    		newrmd=`printf $newname | cut -d \. -f 1`
-   		printf "$newrmd.Rmd\n"
-   		cp $name $newname
-   		cp $name old/$name
-   		cd old
-   		pwd
-   		cp ../$redirect $name
-    	printf "$name -> $newname\n"
-	   	perl -p -i -e 's/http:.+?in_R\//'$newname'/g' $name
-	   	cd ..
-	   	pwd
+   		printf "cp $name -> $newname\n"
+   		# cp $name $newname
+   		printf "cp $oldrmd.Rmd -> $newrmd.Rmd\n"
+   		# cp $oldrmd.Rmd $newrmd.Rmd
+   		printf "cp $name -> old/$name\n"
+   		# cp $name old/$name
+   		printf "mv $oldrmd.Rmd -> old/$oldrmd.Rmd\n"
+   		# mv $oldrmd.Rmd old/$oldrmd.Rmd
+   		printf "redirect $name -> $newname\n"
+   		# cp $redirect $name
+	   	# perl -p -i -e 's/http:.+?in_R\//'$newname'/g' $name
    	else
    		printf "$name\n"
    	fi
     
 done < $collate
 
-printf "../$redirect\t../$collate\n"
+printf "$collate\t$redirect\n"
