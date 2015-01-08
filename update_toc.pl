fedf04210f0ea2e1759548b4a254430c08ef79e8
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-01-07 20:38:24 -0800

    Perl script coming together

--- update_toc.pl
+++ update_toc.pl
@@ -3,31 +3,58 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Roman;
+use Scalar::Util qw(looks_like_number);
 
 my $run;
 my $verbose;
 my $toc = "";
 my $html = "";
+my $ext = "";
+my $prefix = "";
+my $junk;
+my $count = 0;
+my $rom = 0;
 sub verb;
 
 GetOptions("run" => \$run,
 		   "verbose" => \$verbose,
 		   "toc=s" => \$toc,
-		   "html=s" => \$html
+		   "html=s" => \$html,
+		   "ext=s" => \$ext
 		  );
 
-print "Options\n\nrun\t$run\nverbose\t$verbose\ntoc\t$toc\nhtml\t$html\n\n";
 open(TOC, "<", "$toc")
 	or die "Cannot open $toc: $!\n";
 
 while (my $line = <TOC>)
 {
 	chomp $line;
-	print $line."\n";
+	if ($line =~ m/start:/)
+	{
+		($junk, $line) = split /:/, $line;
+		$count = looks_like_number($line) ? $line : arabic($line);
+		$rom = isroman($line) ? 1 : undef;
+	}
+	elsif ($line =~ m/prefix:/)
+	{
+		($junk, $prefix) = split /:/, $line;
+	} 
+	elsif (length($line) > 1)
+	{
+		my $outcount = $rom ? roman($count) : $count;
+		print "$prefix$outcount\t$line\n";
+		$count++;
+	}
+	else
+	{
+		print "\n";
+	}
+	
+
 }
 
 close(TOC);
-
 sub verb {
 	return unless $verbose; print "@_\n";
 }
