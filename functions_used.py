f8a47dabbc1c21137d23cd6959005f9ff2c2535d
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-07-14 21:09:56 -0700

    gather all R chunks from files.

--- functions_used.py
+++ functions_used.py
@@ -13,19 +13,17 @@ poppr = rpackages.importr('poppr')
 pegas = rpackages.importr('pegas')
 adegenet = rpackages.importr('adegenet')
 ape = rpackages.importr('ape')
-ggplot2 = rpackages.importr('ggplot2')
 mmod = rpackages.importr('mmod')
 magrittr = rpackages.importr('magrittr')
 treemap = rpackages.importr('treemap')
 
-pkgs = ["poppr", "pegas", "adegenet", "ape", "ggplot2", "mmod", "magrittr", "treemap"]
+pkgs = ["poppr", "pegas", "adegenet", "ape", "mmod", "magrittr", "treemap"]
 
 robjects.r('''
         poppr.funks <- ls('package:poppr')
         pegas.funks <- ls('package:pegas')
 		adegenet.funks <- ls('package:adegenet')
 		ape.funks <- ls('package:ape')
-		ggplot2.funks <- ls('package:ggplot2')
 		mmod.funks <- ls('package:mmod')
 		magrittr.funks <- ls('package:magrittr')
 		treemap.funks <- ls('package:treemap')
@@ -41,9 +39,44 @@ for package in pkgs:
 		funkdict[funk] = 0
 	pkgdict[package] = funkdict
 
-for package in pkgdict.keys():
-	pkg = pkgdict[package]
-	print(pkg.keys())
 
-# make list of lists
-# add functions to list by grabbing with robjects.r['package.funks']
+# Scan through files and save the code chunks in list
+# make another loop through pkgdict and write the appendix by package.
+
+# Read in toc.txt
+toc = io.open("toc.txt")
+tocdict = dict()
+startin = False
+for line in toc:
+	line.strip()
+	if re.match("Introduction", line):
+		startin = True
+	if not line.strip():
+		startin = False
+		next
+	if startin == True:
+		tocdict[line.strip()] = list()
+
+toc.close()
+
+for chapter in tocdict.keys():
+	chapter_file = io.open(chapter + u'.Rmd')
+	is_chunk = False
+	for line in chapter_file:
+		chunk_start = re.match(r'```\{r', line)
+		chunk_end = re.match(r'```\n', line)
+		if chunk_start:
+			is_chunk = True
+
+		if is_chunk and chunk_end: 
+			is_chunk = False
+
+		if is_chunk:
+			tocdict[chapter].append(line.strip())
+
+	chapter_file.close()
+
+
+# for package in pkgdict.keys():
+# 	pkg = pkgdict[package]
+# 	print(pkg.keys())
