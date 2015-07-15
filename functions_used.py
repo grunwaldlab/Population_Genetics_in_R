9dfbfb3e3116c2f5733a8b53231a8f9ef048287a
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-07-15 11:47:56 -0700

    add some documentation to python script

--- functions_used.py
+++ functions_used.py
@@ -1,14 +1,92 @@
 #!/usr/bin/env python2.7
 
-import io
-import sys 
+
+'''
+Description:
+
+	functions_used.py is a script designed specifically for the Population
+	Genetics in R webpage. It has no guarantee of doing anything remotely useful
+	outside of this page.
+
+Motivation: 
+
+	Create a script that will find all of the user-facing function calls within
+	the Rmd documents and render an Rmd document to serve as an appendix of all
+	the functions used in the primer.
+
+Execution:
+	
+	This utilizes rpy2 in order to import the R packages and get a list of
+	objects in their respective namespaces. It then compiles a dictionary where
+	the keys are function names and the values are the packages where they were
+	first found. This means that overlapping namespaces will take the first
+	package always. After this, it reads the toc.txt file and grabs all of the
+	main chapters, placing them into a list and a dictionary. Each dictionary
+	will contain a list in which all the lines within code chunks will be
+	stored. After the code chunks are stored in the chapter dictionary, it is
+	once more iterated over and each line not starting with an escape is
+	analyzed with find_functions. Functions are detected by searching for an
+	opening round brace. These are placed into a set as a markdown table row
+	with the function name and the link to the R package. Each chapter is added
+	to another dictionary for the chapters and then the chapter list is iterated
+	over in order and the functions are written to a table in the output file.
+'''
+
+
+import io 
 import re 
-import getopt 
-import os
 import rpy2
 import rpy2.robjects as robjects
 import rpy2.robjects.packages as rpackages
 
+'''
+Try to find all the functions in a line of code.
+
+line      - a unicode string of code
+functions - a dictionary containing keys of function names and package names as
+			values
+outset	  - a set in which to add the output
+'''
+def find_functions(line, functions, outset):
+	words = line.strip().split(u' ')
+	# print(words)
+	for word in words:
+		if len(word) < 1:
+			next
+		funks = word.split(u'(')
+		if len(funks) > 1:
+			for funk in funks:
+				if functions.has_key(funk):
+					pkg = functions[funk]
+					funk_out = u'**`' + funk + u'`** | [*' + pkg + u'*](http://cran.r-project.org/package=' + pkg + u') |'
+					# print(funk_out)
+					outset.add(funk_out)
+		else:
+			next
+	return(outset)
+
+'''
+Enter into an Rmd file and grab the name of the chapter.
+
+chapter - the name of the Rmd file without extension.
+'''
+def get_chapter_title(chapter):
+	chapfile = io.open(chapter + u'.Rmd')
+	got_it = False
+	for line in chapfile:
+		if re.match(r'title: ', line, re.UNICODE):
+			the_line = line.strip()
+			the_line = line.split("'")
+			if (len(the_line)) == 1:
+				the_line = line.split("\"")
+			the_line = the_line[1]
+			break
+	chapfile.close()
+	return(the_line)
+
+
+# Load all of the R packages and grab their namespaces
+#-------------------------------------|
 poppr = rpackages.importr('poppr')
 pegas = rpackages.importr('pegas')
 adegenet = rpackages.importr('adegenet')
@@ -39,41 +117,8 @@ for package in pkgs:
 			if not functions.has_key(funk):
 				functions[funk] = package
 
-def find_functions(line, functions, outset):
-	words = line.strip().split(u' ')
-	# print(words)
-	for word in words:
-		if len(word) < 1:
-			next
-		funks = word.split(u'(')
-		if len(funks) > 1:
-			for funk in funks:
-				if functions.has_key(funk):
-					pkg = functions[funk]
-					funk_out = u'**`' + funk + u'`** | [*' + pkg + u'*](http://cran.r-project.org/package=' + pkg + u') |'
-					# print(funk_out)
-					outset.add(funk_out)
-		else:
-			next
-	return(outset)
-
-def get_chapter_title(chapter):
-	chapfile = io.open(chapter + u'.Rmd')
-	got_it = False
-	for line in chapfile:
-		if re.match(r'title: ', line, re.UNICODE):
-			the_line = line.strip()
-			the_line = line.split("'")
-			if (len(the_line)) == 1:
-				the_line = line.split("\"")
-			the_line = the_line[1]
-			break
-	chapfile.close()
-	return(the_line)
-
-# make another loop through functions and write the appendix by package.
-
 # Read in toc.txt
+#-------------------------------------|
 toc = io.open("toc.txt")
 chapters = dict()
 chapterlist = list()
@@ -93,6 +138,7 @@ for line in toc:
 toc.close()
 
 # Scan through files and save the code chunks in list
+#-------------------------------------|
 for chapter in chapters.keys():
 	chapter_file = io.open(chapter + u'.Rmd')
 	its_a_chunk = False
@@ -112,6 +158,8 @@ for chapter in chapters.keys():
 
 	chapter_file.close()
 
+# Create a dictionary of the functions for each chapter
+#-------------------------------------|
 table_of_fun = dict()
 for chapter in chapters.keys():
 	funset = set()
@@ -122,12 +170,18 @@ for chapter in chapters.keys():
 			funlist = find_functions(line, functions, funset)
 	table_of_fun[chapter] = funset
 
+# Open a new file and write the header
+#-------------------------------------|
 funpendix = io.open("funpendix.Rmd", "w")
 funpendix.writelines([u'---\n', u'title: "A2: Function Glossary"\n', u'---\n'])
 funpendix.writelines([u'\n\nBelow are functions that were utilized in the making ',
 					u'of this primer with links to their respective packages.', 
 					u' Note that this list was automatically generated and might '
 					u'be missing some functions.', u'\n\n'])
+
+# Move through the chapters in order and print the section header with the
+# function table.
+#-------------------------------------|
 for chapter in chapterlist:
 	the_title = get_chapter_title(chapter)
 	funpendix.writelines(u'### [' + the_title + u'](' + chapter + u'.html)\n\n')
@@ -144,6 +198,3 @@ for chapter in chapterlist:
 	funpendix.writelines(u'\n')
 
 funpendix.close()
-# for package in pkgdict.keys():
-# 	pkg = pkgdict[package]
-# 	print(pkg.keys())
